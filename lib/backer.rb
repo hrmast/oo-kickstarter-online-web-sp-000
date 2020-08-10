355ad60393a58a1c83ae3c14ae5e858a046800aa
Heath Mast <hrmast@gmail.com>
2020-08-10 23:23:00 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,12 +2,11 @@ require "pry"
 
 class Backer
 
-attr_reader :backed_projects
+  attr_reader :backed_projects
 
-def initialize
-  @backed_projects = []
-  
-end
+  def initialize
+    @backed_projects = []
+  end
 
 
 end
